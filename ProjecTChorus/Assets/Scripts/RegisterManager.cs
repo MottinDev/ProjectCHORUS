bc5c81ccbaec17bc9dabf67b505f8cecf1358a5e
OtnielSSilva <otniel.dss04@gmail.com>
2025-10-25 16:32:33 -0400

    feat: implementa cenas de login e cadastro com a lógica de autenticação dos players

---
+++ ProjecTChorus/Assets/Scripts/RegisterManager.cs
@@ -0,0 +1,69 @@
+using System.Threading.Tasks;
+using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RegisterManager : AuthBase
+{
+    [Header("UI do Cadastro")]
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+    public TMP_InputField confirmPasswordInput;
+
+    /* Chama o Start() do AuthBase, que não escuta o evento SignedIn */
+
+    public async void OnRegisterPressed()
+    {
+        string username = usernameInput.text;
+        string password = passwordInput.text;
+        string confirmPassword = confirmPasswordInput.text;
+
+        // Validação de senha
+        if (password != confirmPassword)
+        {
+            Debug.LogError("As senhas não conferem!");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        {
+            Debug.LogError("Usuário e senha não podem ser vazios.");
+            return;
+        }
+
+        try
+        {
+            await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(username, password);
+
+            Debug.Log($"Cadastro bem-sucedido para o usuário: {username}");
+
+            AuthenticationService.Instance.SignOut();
+
+            Debug.Log("Cadastro completo! Redirecionando para a tela de Login.");
+            SceneManager.LoadScene("LoginScene");
+        }
+        catch (AuthenticationException ex)
+        {
+            if (ex.ErrorCode == AuthenticationErrorCodes.AccountAlreadyLinked)
+            {
+                Debug.LogError("Este nome de usuário já existe.");
+            }
+            else
+            {
+                Debug.LogException(ex);
+            }
+        }
+        catch (RequestFailedException ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+
+    public void OnBackToLoginPressed()
+    {
+        SceneManager.LoadScene("LoginScene");
+    }
+}
